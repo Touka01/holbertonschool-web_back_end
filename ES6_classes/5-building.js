42a6d7c13c05abb934252cf3b6d3a180f842fa4f
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-09-17 12:29:26 +0000

    build

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+    constructor(sqft) {
+      if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+      if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
+      this._sqft = sqft;
+    }
+  
+    /* getter sqft */
+    get sqft() {
+      return this._sqft;
+    }
+  
+    /* setter sqft */
+    set sqft(value) {
+      if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
+      this._sqft = value;
+    }
+  }
