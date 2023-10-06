ff56197bbb6dc93a39bda96a319daabefa32f983
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-10-06 17:07:34 +0000

    lint fix

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,14 @@
 function getListStudentIds(studentList) {
-    // Check if the input is an array
     if (!Array.isArray(studentList)) {
       return [];
     }
   
-    // Use the map function to extract the IDs
     const studentIds = studentList.map((student) => {
       return student.id;
     });
   
     return studentIds;
   }
+  
+  export default getListStudentIds;
+  
