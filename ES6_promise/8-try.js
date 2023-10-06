61938dd6d1ed25892fa194c09b336471461b8f4f
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-10-06 15:24:22 +0000

    letstry

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,3 @@
-export default function divideFunction(numerator, denominator) {
-    if (denominator === 0) {
-      throw Error('Cannot divide by 0');
-    }
-  
-    return numerator / denominator;
-  }
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
+}
