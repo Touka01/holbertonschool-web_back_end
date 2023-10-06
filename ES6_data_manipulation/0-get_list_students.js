85e88a3a069f84cb0828867db89a7f81fc68e5fd
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-10-06 16:59:14 +0000

    0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 function getListStudents() {
-    return [
-      { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
-      { firstName: 'James', id: 2, location: 'Columbia' },
-      { firstName: 'Serena', id: 5, location: 'San Francisco' },
-    ];
-  }
-  
-  export default getListStudents;
+  return [
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
+
+export default getListStudents;
