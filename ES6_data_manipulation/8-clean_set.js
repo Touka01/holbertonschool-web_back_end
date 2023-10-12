bd3e750fb08ef7b219a8c3922aeddcd1e44ea759
Ahmed Toukebri <ahmedtoukebri0@gmail.com>
2023-10-12 10:12:09 +0000

    CFix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,16 @@
 export default function cleanSet(set, startString) {
-  const result = [];
+  const cleanedValues = [];
 
-  if (typeof startString !== 'string' || typeof set !== 'object' || startString.length === 0) {
+  if (
+    typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0
+  ) {
     return '';
   }
 
   for (const item of set) {
-    result.push(item.slice(startString.length));
+    if (item && item.startsWith(startString)) {
+      cleanedValues.push(item.slice(startString.length));
+    }
   }
-  return result.join('-');
+  return cleanedValues.join('-');
 }
